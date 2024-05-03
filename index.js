3bda3a90416a2327f67d75125f4c15ac043f88b2
DeeBest <simphiwedladla8@gmail.com>
2024-05-03 22:27:19 +0200

    added function to increase the width of the bar

--- index.js
+++ index.js
@@ -2,3 +2,24 @@ const currentYear = new Date().getFullYear();
 
 const footerParagraph = document.getElementById('footer-paragraph');
 footerParagraph.textContent = `Created By \xA9Simphiwe ${currentYear}.`;
+
+const container = document.getElementById('container');
+const bar = document.getElementById('bar');
+
+let counter = 0;
+
+updateBar();
+
+function updateBar() {
+  bar.textContent = counter + '%';
+
+  bar.style.width = counter + '%';
+
+  counter++;
+
+  if (counter < 101) {
+    setTimeout(() => {
+      updateBar();
+    }, 1000);
+  }
+}
